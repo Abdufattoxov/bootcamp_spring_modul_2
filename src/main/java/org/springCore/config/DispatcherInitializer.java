0d77d2d1da703805bb5f8c04ad75de0d55e1253d
Shakhzod Abdufattokhov <abdufattoxovshaxzod0404@gmail.com>
2024-05-22 16:25:34 +0500

    spring_mvc is done

---
+++ src/main/java/org/springCore/config/DispatcherInitializer.java
@@ -0,0 +1,20 @@
+package org.springCore.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebMvcConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/java/org/springCore/controller/HomeController.java
@@ -0,0 +1,19 @@
+package org.springCore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home  ")
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        modelAndView.addObject("username", "Shakhzod");
+        return modelAndView;
+    }
+}
